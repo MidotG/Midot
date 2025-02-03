dbc9bd7e337abc1d13b67c3cfae82c74488c53cf
MidotG <shin.alexey1@gmail.com>
2025-02-03 13:50:42 +0300

    C3 - adding settings menu#2.

--- menu.gd
+++ menu.gd
@@ -6,8 +6,11 @@ func _on_start_button_pressed():
 
 
 func _on_options_button_pressed():
-	pass;
-
+	var options = load("res://settings.tscn").instantiate();
+	get_tree().current_scene.add_child(options);
+# // Ыаа ыааа поверх главного меню настройки лезут, в меню и настройках добавить задний фон.
+	#get_tree().change_scene_to_file("res://settings.tscn");
+	
 
 func _on_quit_button_pressed():
 	pass # Replace with function body.
