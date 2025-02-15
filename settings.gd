c55303996f6219415b4e4d667824d042f8be6a5d
MidotG <shin.alexey1@gmail.com>
2025-02-15 23:55:13 +0300

    C4 - fixing checkBoxButton window mode.

--- settings.gd
+++ settings.gd
@@ -1,11 +1,24 @@
 extends Control
 
+@onready var checkBoxButton = $VBoxContainer/HBoxContainer/CheckBox;
+#/// Исправить траюлу, при первом нажатии на кнопку, игра на становится в режиме окна.
+
+func _ready():
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+		checkBoxButton.button_pressed = true;
+	elif DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		checkBoxButton.button_pressed = false;
+	
+	
 func _on_quit_settings_button_pressed():
 	get_tree().change_scene_to_file("res://menu.tscn");
 	
 
 func _on_check_box_toggled(toggled_on):
 	if toggled_on == true:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN);
-	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED);
+		DisplayServer.window_set_size(Vector2i(1280, 720));
+		get_window().move_to_center();
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN);
+		
--- world.gd
+++ world.gd
@@ -3,4 +3,9 @@ extends Node2D
 func _ready():
 	MusicPlayer.play_track(1);
 
+#func _process(delta):
+	#if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		#print("FULLSCREEN");
+	#else:
+		#print(DisplayServer.window_get_mode());
 
