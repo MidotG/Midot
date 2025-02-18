e0624faa97e24fcb2f926d3c2c49331ca99c6ef5
MidotG <shin.alexey1@gmail.com>
2025-02-18 17:09:51 +0300

    C7 - didn't find fixing of this problem, just changed resolution quality.

--- settings.gd
+++ settings.gd
@@ -15,8 +15,8 @@ func _on_quit_settings_button_pressed():
 
 func _on_check_box_toggled(toggled_on):
 	if toggled_on == true:
-		get_window().mode = Window.MODE_WINDOWED;
-		get_viewport().size = Vector2i(1280,720);
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED);
+		get_viewport().size = Vector2i(1600,900);
 		get_window().move_to_center();
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN);
