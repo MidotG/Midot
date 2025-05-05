a9377beb7eed6f4ab2eed96a5df3638a760327a5
MidotG <shin.alexey1@gmail.com>
2025-05-05 18:15:15 +0300

    С11 - removing generation. New.

--- world.gd
+++ world.gd
@@ -1,143 +1,52 @@
 extends Node2D
 
 @onready var player = $Player;
-const tile_size = 2048;
-const chunk_size = 4;
-var loaded_chunks ={};
-var generated_obstacles = {};
+@onready var zombie_spawn_timer = $zombieSpawnTimer;
+@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D;
 
-var location = Vector2();
-var packed_scene =[
-	preload("res://zombie.tscn")
-]
-var canSpawn = true;
-
-#var time_in_s : int = 0;
-#var total_time_in_m : int = 0;
-#var total_time_in_s : int = 0;
-
-
-#///TODO: генерацию препятствий.
+@onready var zombie_scene = preload("res://zombie.tscn");
 
 func _ready():
-	#///TODO: сделать проигрышь всех треков, кроме нулевого по очереди и в рандомном порядке.
-	#///TODO: настроить уровень звука и пр.
-	var spawn_chunk_x = floor(player.global_position.x / (chunk_size * tile_size));
-	var spawn_chunk_y = floor(player.global_position.y / (chunk_size * tile_size));
-	generate_obstacles(spawn_chunk_x, spawn_chunk_y);
-	update_chunks();
-	MusicPlayer.play_track(6);
-	$lvlTimer.start();
-	
+	$lvlTimer.start()
+	zombie_spawn_timer.timeout.connect(_spawn_zombie_inside_polygon);
+
 func _process(delta):
-	update_chunks();
-	if canSpawn:
-		$zombieSpawnTimer.start();
-		canSpawn = false;
+	pass;
 
-
-func _on_zombie_spawn_timer_timeout():
-	canSpawn = true;
-	randomize();
-	#var rnd_index = randi() % $Collisions/CollisionPolygon2D.polygon.size();
-	#location.x = $Collisions/CollisionPolygon2D.polygon[rnd_index].x;
-	#location.y = $Collisions/CollisionPolygon2D.polygon[rnd_index].y;
+func _spawn_zombie_inside_polygon():
+	var polygon = collision_polygon_2d.polygon
+	if polygon.size() < 3:
+		return  # Нельзя спавнить, если полигон не задан
 	
-	#///TODO: карту щас.
-	#///TODO: потом сделать спавн рандомный за экраном игрока.
-	var x = randf_range(-1500, 1500);
-	var y = randf_range(-2000, 2000);
-	location.x = $Player.global_position.x + x;
-	location.y = $Player.global_position.y + y;
-	var scene = packed_scene[0].instantiate();
-	scene.connect("pointsSignal", Callable(self, "add_point"));
-	scene.position = location;
-	add_child(scene);
+	# 1. Находим ограничивающий прямоугольник (AABB) полигона
+	var bounds = _calculate_polygon_bounds(polygon)
 	
-func _on_lvl_timer_timeout():
-	SaveLvl.time_in_s += 1;
-	SaveLvl.total_time_in_s = SaveLvl.time_in_s%60;
-	SaveLvl.total_time_in_m = int(SaveLvl.time_in_s/60.0);
+	var attempts = 0
+	var max_attempts = 50  # Максимум попыток на поиск точки
 	
-func add_point(point : int):
-	SaveLvl.points += point;
-
-
-func update_chunks():
-	var pl_chunk_x = floor(player.global_position.x / (chunk_size * tile_size));
-	var pl_chunk_y = floor(player.global_position.y / (chunk_size * tile_size));
-	
-	# Загрузка и генерация чанков
-	for x in range(pl_chunk_x - 2, pl_chunk_x + 3):  # Увеличил радиус загрузки чанков
-		for y in range(pl_chunk_y - 2, pl_chunk_y + 3):
-			if not is_chunk_loaded(x, y):
-				load_chunk(x, y);
-				generate_obstacles(x, y);  # Генерация препятствий для нового чанка
-	
-	# Выгрузка чанков
-	for chunk_coords in loaded_chunks.keys():
-		var chunk_x = chunk_coords.x;
-		var chunk_y = chunk_coords.y;
-		if abs(chunk_x - pl_chunk_x) > 2 or abs(chunk_y - pl_chunk_y) > 2:  # Увеличил радиус выгрузки
-			unload_chunk(chunk_x, chunk_y);
-
-func is_chunk_loaded(x, y):
-	return Vector2(x, y) in loaded_chunks;
-
-func load_chunk(x, y):
-	for i in range(chunk_size):
-		for j in range(chunk_size):
-			var world_x = x * chunk_size + i;
-			var world_y = y * chunk_size + j;
-			$Environment/FloorTileMap.set_cell(0, Vector2i(world_x, world_y), 1, Vector2i(0, 0));
-	loaded_chunks[Vector2(x, y)] = true;
-
-func unload_chunk(x, y):
-	for i in range(chunk_size):
-		for j in range(chunk_size):
-			var world_x = x * chunk_size + i;
-			var world_y = y * chunk_size + j;
-			$Environment/FloorTileMap.set_cell(0, Vector2(world_x, world_y), -1);
-	loaded_chunks.erase(Vector2(x, y));
-
-func generate_obstacles(x, y):
-	if Vector2(x, y) in generated_obstacles:
-		return;  # Препятствия уже сгенерированы для этого чанка
-	
-	var obstacles = [];
-	var camera = player.get_node("Camera2D");  # Получаем камеру игрока
-	var camera_rect = camera.get_viewport_rect();  # Получаем прямоугольник видимости камеры
-	
-	# Переводим прямоугольник видимости в глобальные координаты
-	var camera_global_pos = camera.global_position;
-	var visible_left = camera_global_pos.x - camera_rect.size.x / 2;
-	var visible_right = camera_global_pos.x + camera_rect.size.x / 2;
-	var visible_top = camera_global_pos.y - camera_rect.size.y / 2;
-	var visible_bottom = camera_global_pos.y + camera_rect.size.y / 2;
-	
-	# Максимальное количество препятствий на чанк (1/5 от площади чанка)
-	var max_obstacles = int((chunk_size * chunk_size) / 5);
-	var obstacle_count = 0;
-	
-	# Генерация препятствий
-	while obstacle_count < max_obstacles:
-		var i = randi() % chunk_size;  # Случайная координата X в пределах чанка
-		var j = randi() % chunk_size;  # Случайная координата Y в пределах чанка
-		var world_x = x * chunk_size + i;
-		var world_y = y * chunk_size + j;
+	while attempts < max_attempts:
+		attempts += 1
 		
-		# Переводим координаты тайлов в пиксели
-		var tile_pixel_x = world_x * tile_size + tile_size / 2;  # Центр тайла
-		var tile_pixel_y = world_y * tile_size + tile_size / 2;  # Центр тайла
+		# 2. Генерируем случайную точку внутри AABB
+		var random_point = Vector2(
+			randf_range(bounds.position.x, bounds.end.x),
+			randf_range(bounds.position.y, bounds.end.y)
+		)
 		
-		# Проверка, чтобы препятствия не спавнились в зоне видимости камеры
-		if (tile_pixel_x < visible_left or tile_pixel_x > visible_right or
-			tile_pixel_y < visible_top or tile_pixel_y > visible_bottom):
-			# Проверяем, что на этой позиции ещё нет препятствия
-			if $Environment/ObstacleTileMap.get_cell_source_id(0, Vector2i(world_x, world_y)) == -1:
-				$Environment/ObstacleTileMap.set_cell(0, Vector2i(world_x, world_y), 1, Vector2i(0, 0));
-				obstacles.append(Vector2i(world_x, world_y));
-				obstacle_count += 1;
+		# 3. Проверяем, лежит ли точка внутри полигона
+		if Geometry2D.is_point_in_polygon(random_point, polygon):
+			# 4. Создаём зомби
+			var zombie = zombie_scene.instantiate()
+			zombie.position = random_point
+			add_child(zombie)
+			break  # Выходим, если заспавнили
+
+func _calculate_polygon_bounds(polygon: PackedVector2Array) -> Rect2:
+	var min_point = polygon[0]
+	var max_point = polygon[0]
 	
-	# Сохраняем сгенерированные препятствия
-	generated_obstacles[Vector2(x, y)] = obstacles;
+	for point in polygon:
+		min_point = Vector2(min(min_point.x, point.x), min(min_point.y, point.y))
+		max_point = Vector2(max(max_point.x, point.x), max(max_point.y, point.y))
+	
+	return Rect2(min_point, max_point - min_point)
