624d2c0d54ae3ec3364ed01804502dd0a70a5bd9
MidotG <shin.alexey1@gmail.com>
2025-05-25 06:25:59 +0300

    C10.

--- laser_beam.gd
+++ laser_beam.gd
@@ -1,32 +1,44 @@
 extends Node2D
 
-@export var damage := 30               # Урон за выстрел
+@export var damage := 5               # Урон за выстрел
 @export var color := Color(1, 0, 0)   # Цвет (красный)
-
-#///TODO: исправить траблы, сделать норм. А также с прошлого коммита тудушки чекнуть и сделать.
-
+@export var damage_interval := 1.0    # Интервал между ударами по одному врагу
 
 @onready var line := $Line2D
 @onready var area := $Area2D
 @onready var line_2d_remove_timer = $line2d_remove_timer
 
+var enemy_cooldowns := {}  # Словарь для хранения таймеров врагов
+
 func _ready():
 	line.default_color = color
 	line.hide()
-	
-func _physics_process(delta):
-	pass;
 
 func shoot():
 	line.show()
-	# Мгновенный урон всем врагам на линии
+	
 	for body in area.get_overlapping_bodies():
-		print("body looked");
 		if body.is_in_group("Enemy") and body.has_method("damage"):
+			var enemy_id : int = body.get_instance_id()
+			
+			# Если враг уже в кд - пропускаем
+			if enemy_id in enemy_cooldowns:
+				continue
+				
+			# Наносим урон
 			body.damage(damage)
-			print("damage enemy");
-	line_2d_remove_timer.start();
+			
+			# Добавляем врага в кд
+			enemy_cooldowns[enemy_id] = true
+			
+			# Создаем таймер для сброса кд этого врага
+			var timer := get_tree().create_timer(damage_interval)
+			timer.timeout.connect(_remove_enemy_cooldown.bind(enemy_id))
+	
+	line_2d_remove_timer.start()
 
+func _remove_enemy_cooldown(enemy_id):
+	enemy_cooldowns.erase(enemy_id)
 
 func _on_line_2d_remove_timer_timeout():
-	line.hide();
+	line.hide()
