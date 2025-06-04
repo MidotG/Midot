0e60bdd4cad27cb239c7289675266fe51844a3c0
MidotG <shin.alexey1@gmail.com>
2025-06-04 17:13:36 +0300

    C35.

--- music_player.gd
+++ music_player.gd
@@ -1,31 +1,34 @@
 extends AudioStreamPlayer
 
-@onready var music_player = MusicPlayer; # Получаем ссылку на AudioStreamPlayer
+@export var music_tracks : Array[AudioStream] = []; 
+var current_track_index = -1;
 
-# Массив для хранения музыкальных треков (AudioStream ресурсов)
-@export var music_tracks : Array[AudioStream] = []; # Используем @export для удобства заполнения в редакторе, тип Array[AudioStream]
-
-var current_track_index = -1 # Индекс текущего играющего трека
-
-#func _ready():
-	#if music_tracks.size() > 0:
-		#play_track(0) # Начнем играть первый трек при запуске (опционально)
+func _ready():
+	if not finished.is_connected(_on_music_finished):
+		finished.connect(_on_music_finished, CONNECT_REFERENCE_COUNTED)
 
 func play_track(track_index : int):
-	if current_track_index == track_index:  #///TODO: проверить потом музыку когда настройки и стоп меню будут во время лвла.
+	if current_track_index == track_index:
 		return;
 	if track_index >= 0 and track_index < music_tracks.size():
 		current_track_index = track_index
-		music_player.stream = music_tracks[current_track_index]
-		music_player.play()
-		print("Музыка: Начало воспроизведения трека ", current_track_index)
+		stream = music_tracks[current_track_index]
+		play()
+		print("Музыка: Начало воспроизведения трека ", current_track_index);
+		#print(str(current_track_index) + "ТЕКУЩИЙ КУРЕНТ ТРЭК ИНДЕКС МЕТОД ПОСЛЕ НАЧАЛА`");
 	else:
-		printerr("Ошибка: Неверный индекс музыкального трека: ", track_index)
+		printerr("Ошибка: Неверный индекс музыкального трека: ", track_index);
+
+func _on_music_finished():
+	#print(str(current_track_index) + "ТЕКУЩИЙ КУРЕНТ ТРЭК ИНДЕКС МЕТОД ПОСЛЕ ЗАВЕРШЕНИЯ");
+	var cur_track_index = current_track_index;
+	current_track_index = -1;
+	play_track(cur_track_index);
 
 func stop_music():
-	music_player.stop()
+	stop();
 	print("Музыка: Остановлено воспроизведение")
 
-
 func get_current_track_index():
-	return current_track_index
+	return current_track_index;
+
