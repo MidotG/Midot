21b0c1f1f34d54cb5462b7c9d7492912e2650dff
MidotG <shin.alexey1@gmail.com>
2025-06-05 17:14:29 +0300

    C38.

--- automatic_rifle.gd
+++ automatic_rifle.gd
@@ -25,7 +25,7 @@ func shoot(position):
 		bul.dir = (position - get_global_mouse_position()).normalized();
 		get_tree().root.add_child(bul);
 		bul.rotation_degrees = rad_to_deg(global_position.angle_to_point(global_position + bul.dir));
-		bul.position = position;
+		bul.position = $Marker2D.global_position;
 
 func _on_shoot_speed_timer_timeout():
 	canShoot = true;
--- laser_gun.gd
+++ laser_gun.gd
@@ -1,13 +1,8 @@
 extends Node2D
 
-#const laser_beam = preload("res://laser_beam.tscn");
 @onready var laser_beam = $laser_beam;
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	#print(laser_beam.damage);
-	#print("nigga");
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
